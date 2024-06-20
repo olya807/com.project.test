9fa88e0fced22e5795b129e6130acdd3a0b0bb3e
Volha <morozolga92@gmail.com>
2024-06-20 14:05:06 +0300

    BubbleSortExample
    This code sorts an array of integers in ascending order using the bubble sort algorithm.

--- src/BubbleSortExample.java
+++ src/BubbleSortExample.java
@@ -1,2 +1,26 @@
 public class BubbleSortExample {
+    
+    public static void main(String[] args) {
+        int[] array = {3, 60, 35, 2, 45, 320, 5};
+        bubbleSort(array);
+        System.out.println("Отсортированный массив:");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (array[j - 1] > array[j]) {
+//swap elements
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
 }
