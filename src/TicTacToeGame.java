7602b870984cc289efb6f533314c639037f196d4
Volha <morozolga92@gmail.com>
2024-06-22 21:08:04 +0300

    This version of the game added methods for checking winning conditions by rows, columns and diagonals. Now the game ends when one of the players wins or when all the cells are full, in which case a draw is declared.

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -34,6 +34,36 @@ public class TicTacToeGame {
         return true;
     }
 
+    public boolean checkForWin() {
+        return (checkRowsForWin() || checkColumnsForWin() || checkDiagonalsForWin());
+    }
+
+    private boolean checkRowsForWin() {
+        for (int i = 0; i < 3; i++) {
+            if (checkRowCol(board[i][0], board[i][1], board[i][2])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkColumnsForWin() {
+        for (int i = 0; i < 3; i++) {
+            if (checkRowCol(board[0][i], board[1][i], board[2][i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiagonalsForWin() {
+        return ((checkRowCol(board[0][0], board[1][1], board[2][2])) || (checkRowCol(board[0][2], board[1][1], board[2][0])));
+    }
+
+    private boolean checkRowCol(char c1, char c2, char c3) {
+        return ((c1 != EMPTY) && (c1 == c2) && (c2 == c3));
+    }
+
     public void changePlayer() {
         currentPlayerMark = (currentPlayerMark == CROSS) ? NOUGHT : CROSS;
     }
@@ -71,11 +101,20 @@ public class TicTacToeGame {
                 row = scanner.nextInt() - 1;
                 col = scanner.nextInt() - 1;
             } while (!game.placeMark(row, col));
-            game.changePlayer();
-            game.gameEnded = game.isBoardFull(); // This is a simple end condition, you can add more!
+
+            game.gameEnded = game.checkForWin() || game.isBoardFull();
+            if (game.gameEnded) {
+                if (game.checkForWin()) {
+                    game.changePlayer(); // To display the correct winner
+                    System.out.println("Player " + game.currentPlayerMark + " wins!");
+                } else {
+                    System.out.println("Game over! It's a tie!");
+                }
+            } else {
+                game.changePlayer();
+            }
         }
         scanner.close();
-        System.out.println("Game over! No more moves left.");
         game.printBoard();
     }
 }
