b0aa727521d3bf39ecde738b6c4d86074c67b27c
Volha <morozolga92@gmail.com>
2024-06-23 21:25:48 +0300

    PasswordGenerator This code uses the Random class to generate a random password from a given set of characters.

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -1,2 +1,20 @@
+import java.util.Random;
+
 public class PasswordGenerator {
+    public static void main(String[] args) {
+        int length = 12; // Можно изменить длину пароля
+        System.out.println(generatePassword(length));
+    }
+
+    public static String generatePassword(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder password = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            password.append(characters.charAt(random.nextInt(characters.length())));
+        }
+
+        return password.toString();
+    }
 }
