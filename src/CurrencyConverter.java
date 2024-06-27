06d4c4fead198f492a839aa739ba6be41da82497
Volha <morozolga92@gmail.com>
2024-06-27 21:04:47 +0300

    CurrencyConverter This code uses a static exchange rate, but you can integrate an external API to get up-to-date currency rates.

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class CurrencyConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double rateUSDToEUR = 0.85; // Примерный курс конвертации, нужно обновлять
+
+        System.out.println("Введите сумму в долларах США для конвертации в евро:");
+        double amountInUSD = scanner.nextDouble();
+
+        double amountInEUR = convertUSDtoEUR(amountInUSD, rateUSDToEUR);
+        System.out.printf("Сумма в евро: %.2f EUR%n", amountInEUR);
+    }
+
+    public static double convertUSDtoEUR(double amount, double rate) {
+        return amount * rate;
+    }
 }
