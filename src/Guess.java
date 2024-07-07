caa4427d264628a28bd9a3cd4ce3267f97ffdadd
Volha <morozolga92@gmail.com>
2024-07-07 21:57:52 +0300

    Guess This game asks the user to guess a word by typing letters or trying to guess the whole word. If the letter is present in the word, it opens in the hint. The game continues until the user guesses the word completely.

--- src/Guess.java
+++ src/Guess.java
@@ -1,2 +1,32 @@
+import java.util.Arrays;
+
 public class Guess {
+
+    public Guess() {
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"компьютер", "программа", "java", "игра", "код"};
+        String secretWord = words[(int) (Math.random() * words.length)];
+        char[] hiddenWord = new char[secretWord.length()];
+        Arrays.fill(hiddenWord, '*');
+        System.out.println("Я загадал слово, попробуй угадать его. Вот подсказка: " + new String(hiddenWord));
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        while (!new String(hiddenWord).equals(secretWord)) {
+            System.out.print("Введи букву или слово: ");
+            String guess = scanner.nextLine();
+            if (guess.length() == 1) {
+                for (int i = 0; i < secretWord.length(); i++) {
+                    if (secretWord.charAt(i) == guess.charAt(0)) {
+                        hiddenWord[i] = guess.charAt(0);
+                    }
+                }
+            } else if (guess.equals(secretWord)) {
+                break;
+            }
+            System.out.println("Текущее состояние слова: " + new String(hiddenWord));
+        }
+        System.out.println("Поздравляю, ты угадал слово: " + secretWord + "!");
+        scanner.close();
+    }
 }
